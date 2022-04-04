b6ad3c4323dad676c967c30f20af999521341fe2
admin <g.lyalko@mail.ru>
2022-04-04 21:58:09 +0300

    2) Все размеры и строки сделал ресурсами.

--- app/src/main/java/com/hfad/firstcalculator/Сalculator.java
+++ app/src/main/java/com/hfad/firstcalculator/Сalculator.java
@@ -4,9 +4,4 @@ public class Сalculator {
     double result;
     double number1;
     double number2;
-    Сalculator calculator = new Сalculator ();
-    double calculator (double result, double number1,double number2)
-    {
-
-    }
 }
