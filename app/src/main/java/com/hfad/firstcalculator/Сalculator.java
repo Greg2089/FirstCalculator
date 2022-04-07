3c7dbf0be41620d78b919d184e6a9528b979e04d
admin <g.lyalko@mail.ru>
2022-04-07 20:02:27 +0300

    3) Создал свой стиль и тему

--- app/src/main/java/com/hfad/firstcalculator/Сalculator.java
+++ app/src/main/java/com/hfad/firstcalculator/Сalculator.java
@@ -4,4 +4,9 @@ public class Сalculator {
     double result;
     double number1;
     double number2;
+    void divated () {
+        double result = number1/number2;
+        if (number2!=0);
+    }
 }
+//<item name="materialButtonStyle">@style/StyleNumbers</item>
